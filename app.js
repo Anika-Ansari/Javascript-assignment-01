2d8a1b18a661b982aaa809254644d95a0d2e3a33
Anika Ansari <168868218+Anika-Ansari@users.noreply.github.com>
2024-09-19 20:53:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// Q #01
+ 
+alert("Anika");
+
+
+// Q #02
+var myName=("Anika Ansari");
+
+alert("Hello,my name is "+myName+"& I am a student of SMIT");
+
+console.log("Hello,my name is "+myName+"& I am a student of SMIT");
+
+// Q#03
+var a=(4)
+var b=(2)
+var result=(a++ + --b - --a + b++ +a +b)
+console.log (result)
+
+// Q#05
+
+var Institute = prompt("In which institute do you study?");
+var course =prompt("In what course do you enrolled?");
+var days=prompt("your class days?")
+var age=prompt("how old are you?")
+if (Institute === "SMIT" && course === "Web And App"&& days === "Tuesday & Saturday"&& age >=20) {
+   alert("you are allowed");
+}  
+else if(age<=20){
+    alert("you are not allowed");
+}
+   
+   
+    
+
+
+
+
